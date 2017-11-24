c615dba993e3bb4757e8a8d0a48e97d0e3e2241c
mattcs2401 <mattcs2401@gmail.com>
2017-11-24 13:37:56 +1000

    Init commit.

---
+++ app/src/main/java/mcssoft/com/raceremindermvp/model/Database/Database.java
@@ -0,0 +1,57 @@
+package mcssoft.com.raceremindermvp.model.Database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class Database {
+
+    public Database(Context context) {
+        this.context = context;
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public void destroy() {
+        if(dbHelper != null) {
+            dbHelper.close();
+        }
+        dbHelper = null;
+        if(context != null) {
+            context = null;
+        }
+    }
+
+    public Cursor getRecords(String tableName, @Nullable String[] projection, @Nullable String whereClause, @Nullable String[] selArgs) {
+
+        // sanity checks.
+        if(projection == null) {
+            projection = dbHelper.getProjection(tableName);
+        }
+        if(whereClause == null) {
+            selArgs = null;
+        } else if(selArgs == null) {
+            whereClause = null;
+        }
+
+        Cursor cursor = null;
+        SQLiteDatabase db = dbHelper.getDatabase();
+
+        try {
+            db.beginTransaction();
+            cursor = db.query(tableName, projection, whereClause, selArgs, null, null, null);
+        } catch(Exception ex) {
+            Log.d(context.getClass().getCanonicalName(), ex.getMessage());
+        } finally {
+            if(db != null) {
+                db.endTransaction();
+            }
+            return cursor;
+        }
+    }
+
+    private Context context;
+    private static DatabaseHelper dbHelper;
+    private static volatile Database instance = null;
+}
---
+++ app/src/main/java/mcssoft/com/raceremindermvp/model/Database/Schema.java
@@ -0,0 +1,45 @@
+package mcssoft.com.raceremindermvp.model.Database;
+
+public class Schema {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "RACEREMINDER";
+
+    public static final String RACE_DETAILS = "RACEDETAILS";
+
+    // Table RACEDETAILS columns
+    public static final String COLUMN_ROWID = "_id"; // Note: Has to be like this (upper case _ID ?).
+    public static final String COLUMN_CITY_CODE = "CITY_CODE";
+    public static final String COLUMN_RACE_CODE = "RACE_CODE";
+    public static final String COLUMN_RACE_NUM = "RACE_NUM";
+    public static final String COLUMN_RACE_SEL = "RACE_SEL";
+    public static final String COLUMN_DATE_TIME = "DATE_TIME";
+    // Generic field to indicate a display change is required.
+    public static final String COLUMN_D_CHG_REQ = "D_CHG_REQ";
+    // Generic field that indicates if a notification set for the record.
+    public static final String COLUMN_NOTIFIED = "NOTIFIED";
+
+    // Database table create.
+    public static final String DATABASE_CREATE = "CREATE TABLE "
+            + RACE_DETAILS   + " ("
+            + COLUMN_ROWID     + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_CITY_CODE + " TEXT NOT NULL, "
+            + COLUMN_RACE_CODE + " TEXT NOT NULL, "
+            + COLUMN_RACE_NUM  + " INTEGER NOT NULL, "
+            + COLUMN_RACE_SEL  + " INTEGER NOT NULL, "
+            + COLUMN_DATE_TIME + " INTEGER NOT NULL, "
+            + COLUMN_D_CHG_REQ + " TEXT NOT NULL, "
+            + COLUMN_NOTIFIED  + " TEXT NOT NULL)";
+
+    public static final String SORT_ORDER = COLUMN_DATE_TIME + " ASC, " + COLUMN_RACE_SEL;
+
+    // Where a display change is required.
+    public final static String WHERE_FOR_DCHANGE = COLUMN_D_CHG_REQ + " = ? AND " +
+            COLUMN_DATE_TIME + " < ?";
+
+    // where for meetings to notify.
+    public static final String WHERE_FOR_NOTIFY = COLUMN_NOTIFIED + "='N'";
+
+    // where for which meetings to show.
+    public static final String WHERE_FOR_SHOW = COLUMN_DATE_TIME + " > ?";
+}
