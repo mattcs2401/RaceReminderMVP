9a4f99b83421dedae6e3c4bee28e90457be07dc2
mattcs2401 <03m777c22s>
2017-11-26 21:20:29 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/raceremindermvp/model/Database/Database.java
+++ app/src/main/java/mcssoft/com/raceremindermvp/model/Database/Database.java
@@ -2,31 +2,49 @@ package mcssoft.com.raceremindermvp.model.Database;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-public class Database {
+public class Database extends SQLiteOpenHelper {
 
     public Database(Context context) {
-        dbHelper = new DatabaseHelper(context);
+        super(context, Schema.DATABASE_NAME, null, Schema.DATABASE_VERSION);
+        sqLiteDatabase = this.getWritableDatabase();    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.beginTransaction();
+        try {
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Schema.DATABASE_NAME + "." + Schema.RACE_DETAILS + ";");
+            sqLiteDatabase.execSQL(Schema.DATABASE_CREATE);
+            sqLiteDatabase.setTransactionSuccessful();
+        } catch(SQLException sqle) {
+            Log.d("class:Database","Exception thrown on database create: " + sqle.getMessage());
+        } finally {
+            sqLiteDatabase.endTransaction();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Schema.DATABASE_NAME + "." + Schema.RACE_DETAILS + ";");
     }
 
     public void destroy() {
-        if(dbHelper != null) {
-            dbHelper.close();
-            dbHelper = null;
+        if(sqLiteDatabase != null) {
+            sqLiteDatabase.close();
+            sqLiteDatabase = null;
         }
     }
 
     public Cursor getRecords(String tableName, @Nullable String[] projection, @Nullable String whereClause, @Nullable String[] selArgs) {
 
-        if(dbHelper == null) {
-            return null;
-        }
         // sanity checks.
         if(projection == null) {
-            projection = dbHelper.getProjection(tableName);
+            projection = getProjection(tableName);
         }
         if(whereClause == null) {
             selArgs = null;
@@ -35,21 +53,41 @@ public class Database {
         }
 
         Cursor cursor = null;
-        SQLiteDatabase db = dbHelper.getDatabase();
 
         try {
-            db.beginTransaction();
-            cursor = db.query(tableName, projection, whereClause, selArgs, null, null, null);
+            sqLiteDatabase.beginTransaction();
+            cursor = sqLiteDatabase.query(tableName, projection, whereClause, selArgs, null, null, null);
         } catch(Exception ex) {
             Log.d("class:Database", ex.getMessage());
         } finally {
-            if(db != null) {
-                db.endTransaction();
+            if(sqLiteDatabase != null) {
+                sqLiteDatabase.endTransaction();
             }
             return cursor;
         }
     }
 
-    private static DatabaseHelper dbHelper;
-    private static volatile Database instance = null;
+    public static String [] getProjection(String tableName) {
+        // TODO - custom projection, i.e. not all columns.
+        switch (tableName) {
+            case Schema.RACE_DETAILS:
+                return getRaceDetailsProjection();
+        }
+        return  null;
+    }
+
+    private static final String[] getRaceDetailsProjection() {
+        return new String[] {
+                Schema.COLUMN_ROWID,
+                Schema.COLUMN_CITY_CODE,
+                Schema.COLUMN_RACE_CODE,
+                Schema.COLUMN_RACE_NUM,
+                Schema.COLUMN_RACE_SEL,
+                Schema.COLUMN_DATE_TIME,
+                Schema.COLUMN_D_CHG_REQ,
+                Schema.COLUMN_NOTIFIED
+        };
+    }
+
+    private SQLiteDatabase sqLiteDatabase;
 }
