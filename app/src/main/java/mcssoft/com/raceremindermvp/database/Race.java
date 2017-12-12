46d38350d52af1da6480b1c2f8cbfd6e846b7454
mattcs2401 <03m777c22s>
2017-12-12 16:48:34 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/raceremindermvp/database/Race.java
+++ app/src/main/java/mcssoft/com/raceremindermvp/database/Race.java
@@ -3,10 +3,12 @@ package mcssoft.com.raceremindermvp.database;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 @Entity(tableName = "Race")
-public class Race {
+public class Race implements Parcelable {
 
     //<editor-fold defaultstate="collapsed" desc="Region: Constructors">
 //    public Race() {
@@ -105,6 +107,52 @@ public class Race {
     public void setNotified(String notified) {
         this.notified = notified;
     }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Region: Parcelable">
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(cityCode);
+        dest.writeString(raceCode);
+        dest.writeString(raceNum);
+        dest.writeString(raceSel);
+        dest.writeString(dateTime);
+        dest.writeString(dChgReq);
+        dest.writeString(notified);
+    }
+
+    /**
+     * Retrieving Race data from Parcel object. This constructor is invoked by the method
+     * createFromParcel(Parcel source) of the object CREATOR.
+     **/
+    public Race(Parcel in) {
+        this.id = in.readString();
+        this.cityCode = in.readString();
+        this.raceCode = in.readString();
+        this.raceNum = in.readString();
+        this.raceSel = in.readString();
+        this.dateTime = in.readString();
+        this.dChgReq = in.readString();
+        this.notified = in.readString();
+    }
+
+    public static final Parcelable.Creator<mcssoft.com.raceremindermvp.model.Race> CREATOR = new Parcelable.Creator<mcssoft.com.raceremindermvp.model.Race>() {
+        @Override
+        public mcssoft.com.raceremindermvp.model.Race createFromParcel(Parcel source) {
+            return new mcssoft.com.raceremindermvp.model.Race(source);
+        }
+
+        @Override
+        public mcssoft.com.raceremindermvp.model.Race[] newArray(int size) {
+            return new mcssoft.com.raceremindermvp.model.Race[size];
+        }
+    };
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="Region: Private">
