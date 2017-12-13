4a90425dad8f3388b14bdb9e80e1fe3f3a7af8c1
mattcs2401 <mattcs2401@gmail.com>
2017-12-13 14:56:56 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/raceremindermvp/loader/RaceLoader.java
+++ app/src/main/java/mcssoft/com/raceremindermvp/loader/RaceLoader.java
@@ -45,10 +45,31 @@ public class RaceLoader extends AsyncTaskLoader<List<Race>> {
 
     private List<Race> loadDummyData() {
         lRaces = new ArrayList<>();
-        Race r1 = new Race("1", "B", "R", "1", "10", "01/01/2017 12:00");
-        Race r2 = new Race("2", "B", "R", "2", "11", "01/01/2017 12:40");
+        Race r1 = new Race("1", "B", "R", "1", "01", "01/01/2017 12:00");
+        Race r2 = new Race("2", "B", "R", "2", "02", "01/01/2017 12:30");
+        Race r3 = new Race("3", "B", "R", "3", "03", "01/01/2017 13:00");
+        Race r4 = new Race("4", "B", "R", "4", "04", "01/01/2017 13:30");
+        Race r5 = new Race("5", "B", "R", "5", "05", "01/01/2017 14:00");
+        Race r6 = new Race("6", "B", "R", "6", "06", "01/01/2017 14:30");
+        Race r7 = new Race("7", "B", "R", "7", "07", "01/01/2017 15:00");
+        Race r8 = new Race("8", "B", "R", "8", "08", "01/01/2017 15:30");
+        Race r9 = new Race("9", "B", "R", "9", "09", "01/01/2017 16:00");
+        Race r10 = new Race("10", "B", "R", "10", "10", "01/01/2017 16:30");
+        Race r11 = new Race("11", "B", "R", "11", "11", "01/01/2017 17:00");
+        Race r12 = new Race("12", "B", "R", "12", "12", "01/01/2017 17:30");
+
         lRaces.add(r1);
         lRaces.add(r2);
+        lRaces.add(r3);
+        lRaces.add(r4);
+        lRaces.add(r5);
+        lRaces.add(r6);
+        lRaces.add(r7);
+        lRaces.add(r8);
+        lRaces.add(r9);
+        lRaces.add(r10);
+        lRaces.add(r11);
+        lRaces.add(r12);
         return lRaces;
     }
 
