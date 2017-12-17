060ff10c75289dcbaa78d000db0c000dc837a89f
mattcs2401 <mattcs2401@gmail.com>
2017-12-18 08:10:21 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/raceremindermvp/database/RaceDatabase.java
+++ app/src/main/java/mcssoft/com/raceremindermvp/database/RaceDatabase.java
@@ -4,28 +4,22 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import mcssoft.com.raceremindermvp.interfaces.di.ApplicationContext;
 import mcssoft.com.raceremindermvp.interfaces.room.IRaceDAO;
 
-@Singleton
 public abstract class RaceDatabase extends RoomDatabase {
 
-    @Inject
-    static synchronized public RaceDatabase getInstance(@ApplicationContext Context context) {
+    static synchronized public RaceDatabase getInstance(Context context) {
         if (instance == null) {
             instance = create(context);
         }
         return instance;
     }
 
+    public abstract IRaceDAO getRaceDAO();  // for Room.
+
     private static RaceDatabase create(final Context context) {
         return Room.databaseBuilder(context, RaceDatabase.class, DB_NAME).build();
     }
-
-    public abstract IRaceDAO getRaceDAO();
 
     private static final String DB_NAME = "RaceDatabase.db";
     private static volatile RaceDatabase instance;
