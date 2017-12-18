f9f99550b8c233d23c7b7ba11f8814343f6f0286
mattcs2401 <mattcs2401@gmail.com>
2017-12-18 13:54:20 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/raceremindermvp/database/RaceDatabase.java
+++ app/src/main/java/mcssoft/com/raceremindermvp/database/RaceDatabase.java
@@ -1,11 +1,14 @@
 package mcssoft.com.raceremindermvp.database;
 
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
 import mcssoft.com.raceremindermvp.interfaces.room.IRaceDAO;
+import mcssoft.com.raceremindermvp.model.Race;
 
+@Database(entities = {Race.class}, version=1)
 public abstract class RaceDatabase extends RoomDatabase {
 
     static synchronized public RaceDatabase getInstance(Context context) {
