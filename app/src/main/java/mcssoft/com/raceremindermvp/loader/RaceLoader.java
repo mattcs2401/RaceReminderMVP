4a626f75da9db5727ea8642a6deb8f32d9789710
mattcs2401 <mattcs2401@gmail.com>
2018-01-09 14:54:43 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/raceremindermvp/loader/RaceLoader.java
+++ app/src/main/java/mcssoft/com/raceremindermvp/loader/RaceLoader.java
@@ -4,40 +4,100 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.os.Bundle;
+import android.util.Log;
+
+import mcssoft.com.raceremindermvp.database.RaceDatabase;
+import mcssoft.com.raceremindermvp.interfaces.ILoaderRace;
 
 public class RaceLoader extends AsyncTaskLoader<Object> {
 
-    public RaceLoader(Context context, Bundle args) {
+    public RaceLoader(Context context, RaceDatabase raceDatabase, Bundle args, ILoaderRace iLoaderRace) {
         super(context);
         this.args = args;
+        this.iLoaderRace = iLoaderRace;
+        this.raceDatabase = raceDatabase;
     }
 
     @Override
     protected void onStartLoading() {
-        super.onStartLoading();
+//        super.onStartLoading();
+        if(data != null) {
+            deliverResult(data);
+        }
+        if (takeContentChanged() || data == null) {// || configChange) {
+            // If the data has changed since the last time it was loaded
+            // or is not currently available, start a load.
+            forceLoad();
+        }
     }
 
     @Override
     public Object loadInBackground() {
-        return null;
+        Log.d("loadInBackGround", "in loadinInBackground");
+        // testing only.
+        Object object = new Object();
+        try {
+            object = raceDatabase.getMeetingDAO().selectAll("N");
+//        return null;
+        } catch (Exception ex) {
+            Log.d("loadInBackground: ", ex.getMessage());
+        }
+        finally {
+            return object;
+        }
     }
 
     @Override
     public void deliverResult(Object data) {
-        super.deliverResult(data);
+        if(isReset()) {
+            if(data != null) {
+                onReleaseResources(data);
+            }
+        }
+
+        Object oldData = data;
+        this.data = data;
+
+        if(isStarted()) {
+            super.deliverResult(data);
+        }
+        if(oldData != null) {
+            onReleaseResources(oldData);
+        }
+        iLoaderRace.deliverResult(data);
     }
 
     @Override
     protected void onStopLoading() {
-        super.onStopLoading();
+//        super.onStopLoading();
+        cancelLoad();
+    }
+
+    @Override
+    public void onCanceled(Object data) {
+        super.onCanceled(data);
+        onReleaseResources(data);
     }
 
     @Override
     protected void onReset() {
         super.onReset();
+        onStartLoading();
+        if(data != null) {
+            onReleaseResources(data);
+            data = null;
+        }
     }
 
+    protected void onReleaseResources(Object object) {
+        // TBA
+        object = null;
+    }
+
+    private Object data;
     private Bundle args;
+    private ILoaderRace iLoaderRace;
+    private RaceDatabase raceDatabase;
 }
 /*
 https://developer.android.com/reference/android/content/AsyncTaskLoader.html
--- app/src/main/java/mcssoft/com/raceremindermvp/loader/RaceLoaderManager.java
+++ app/src/main/java/mcssoft/com/raceremindermvp/loader/RaceLoaderManager.java
@@ -4,25 +4,28 @@ import android.app.LoaderManager;
 import android.content.Loader;
 import android.os.Bundle;
 
+import mcssoft.com.raceremindermvp.database.RaceDatabase;
+import mcssoft.com.raceremindermvp.interfaces.ILoaderRace;
 import mcssoft.com.raceremindermvp.interfaces.IModelLoader;
 import mcssoft.com.raceremindermvp.interfaces.mvp.IPresenterModel;
 
-public class RaceLoaderManager implements LoaderManager.LoaderCallbacks {
+public class RaceLoaderManager implements LoaderManager.LoaderCallbacks, ILoaderRace {
 
     public RaceLoaderManager(IPresenterModel iPresenterModel, IModelLoader iModelLoader) {
         this.iPresenterModel = iPresenterModel;
         this.iModelLoader = iModelLoader;
     }
 
-    public void initLoader(Bundle args) {
-        this.args = args; // save local so we can (update)/pass back to Model.
+    public void initLoader(RaceDatabase raceDatabase, Bundle args) {
+        this.args = args;                   // local copy so we can (update)/pass back to Model.
+        this.raceDatabase = raceDatabase;   // local copy, passed to RaceLoader.
         iPresenterModel.getActivity().getLoaderManager().initLoader(0, args, this);
     }
 
     //<editor-fold defaultstate="collapsed" desc="Region: LoaderCallbacks">
     @Override
     public Loader onCreateLoader(int id, Bundle args) {
-        return new RaceLoader(iPresenterModel.getContext(), args);
+        return new RaceLoader(iPresenterModel.getContext(), raceDatabase, args, this);
     }
 
     @Override
@@ -34,12 +37,21 @@ public class RaceLoaderManager implements LoaderManager.LoaderCallbacks {
     public void onLoaderReset(Loader loader) { }
     //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="Region: ILoaderRace">
+    @Override
+    public void deliverResult(Object data) {
+
+        String bp = "";
+    }
+    //</editor-fold>
+
+    // TBA
     public enum LoaderTasks {
         CHECK_FOR_MEETINGS, INSERT_MEETING, INSERT_MEETINGS, UPDATE_MEETING
     }
 
     private Bundle args;
-    private boolean meetingsExist;
+    private RaceDatabase raceDatabase;
     private IPresenterModel iPresenterModel;
     private IModelLoader iModelLoader;
 }
